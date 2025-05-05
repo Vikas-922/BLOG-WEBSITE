0c9bc22541c6c46ef64012ac9b2250535e387f22
Vikas-922 <vikasvish.9922@gmail.com>
2025-05-05 18:00:29 +0530

    Fix CORS configuration to ensure origins are correctly set as an array

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CORS_ORIGINS,
+  origin: [ process.env.CORS_ORIGINS ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true
 }));
