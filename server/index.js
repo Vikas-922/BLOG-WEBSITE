7642af4e32b288019e462c3cf62c2099998c4e64
Vikas-922 <vikasvish.9922@gmail.com>
2025-05-05 17:51:52 +0530

    minor change

--- server/index.js
+++ server/index.js
@@ -17,7 +17,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGINS,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  credentials: true
+}));
+
 app.use(bodyParser.json());
 app.use(fileUpload({ useTempFiles: true }));
 
